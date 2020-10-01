3e7435301f0d9fd612bad827cd01b32a79f436c5
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-10-01 20:14:46 -0300

    Sistema de adicionar card finalizado

--- src/script.js
+++ src/script.js
@@ -9,40 +9,53 @@ btn.onclick = () => {
     let local = localStorage.getItem(STORAGE_NAME);
     let name = document.getElementById('name').value;
     let url = document.getElementById('url').value;
+    let image = document.getElementById('image').value;
 
     let arr = [];
     if(local == null){
-        arr = [name, url];
+        arr = [name, url, image];
     }else{
-        arr = [local, name, url];
+        arr = [local, name, url, image];
     }
-    localStorage.setItem('cardsteste', arr);
+    localStorage.setItem(STORAGE_NAME, arr);
 
-    addCard(name, url);
+    addCard(name, url, image);
 }
 
 function getItems(){
-    let local = localStorage.getItem(STORAGE_NAME);
-    if(local === null) return;
-    local = local.split(',');
+    let dados = localStorage.getItem(STORAGE_NAME);
+    if(dados === null) return;
+    let local = dados.split(',');
     let name;
     let url;
+    let image;
     
     local.map((item, index) => {
-        if(index % 2 === 0){
+        if(index % 3 === 0){
            name = item;
            url = local[index + 1];
-           addCard(name, url);
+           image = local[index + 2];
+           addCard(name, url, image);
         }
     });
 }
 
-function addCard(name, url){
-    let cardsDiv = document.getElementById(STORAGE_NAME);
+function addCard(name, url, image){
+    let cardsDiv = document.getElementById('cards');
     let linkElement = document.createElement('a');
+    let imgElement = document.createElement('img');
+    let h1Element = document.createElement('h1');
     let linkText = document.createTextNode(name);
 
-    linkElement.appendChild(linkText);
     linkElement.href = url;
+    imgElement.src = image;
+
+    h1Element.appendChild(linkText);
+
+    linkElement.appendChild(imgElement);
+    linkElement.appendChild(h1Element);
+
+    linkElement.classList.add('link');
+
     cardsDiv.appendChild(linkElement);
 }
