d1473985110814dfb5aa39bee72b5f105e38242b
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-10-01 20:43:17 -0300

    Adicionado relógio

---
+++ src/clock.js
@@ -0,0 +1,27 @@
+setInterval(function(){
+    let week = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+
+
+    let novaHora = new Date();
+
+    let hora = novaHora.getHours();
+    let minuto = novaHora.getMinutes();
+    let segundo = novaHora.getSeconds();
+    let week_day = novaHora.getDay();
+
+
+    hora = zero(hora);
+    minuto = zero(minuto);
+    segundo = zero(segundo);
+    
+    document.getElementById('week_day').innerHTML = week[week_day];
+    document.getElementById('hours').innerHTML = hora;
+    document.getElementById('minutes').innerHTML = minuto;
+    document.getElementById('seconds').innerHTML = segundo;
+},1000)
+
+function zero(x) {
+    if (x < 10) {
+        x = '0' + x;
+    } return x;
+}
