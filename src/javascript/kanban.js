e3340751e548500c5d1c4d98e07e86a66b3dbe35
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-10-04 21:32:07 -0300

    Sistema de salvar os cards

--- src/javascript/kanban.js
+++ src/javascript/kanban.js
@@ -1,18 +1,78 @@
+let todo = [];
+let doing = [];
+let done = [];
+
+let fktodo = [];
+let fkdoing = [];
+let fkdone = [];
+
+//Start function
+(() => {
+    let savedTodos = localStorage.getItem('todo');
+    if(savedTodos !== null){
+        var formatedTodos = savedTodos.split(',');
+    }
+
+    let savedDoing = localStorage.getItem('doing')
+    if(savedDoing !== null){
+        var formatedDoing = savedDoing.split(',');
+    }
+
+    let savedDone = localStorage.getItem('done')
+    if(savedDone !== null){
+        var formatedDone = savedDone.split(',');
+    }
+
+    let todoDiv = document.getElementById('todo');
+    let doingDiv = document.getElementById('doing');
+    let doneDiv = document.getElementById('done');
+
+    loadKanBan(formatedTodos, todoDiv);
+    loadKanBan(formatedDoing, doingDiv);
+    loadKanBan(formatedDone, doneDiv);
+    
+})()
+
+function loadKanBan(arr, div){
+    if(arr === undefined) return;
+
+    arr.map(item => {
+        if(item === ""){
+
+        }else{
+            let boardDiv = document.createElement('div');
+            let pEl = document.createElement('p');
+            let pText = document.createTextNode(item);
+    
+            boardDiv.setAttribute('class', 'bloco');
+            boardDiv.setAttribute('draggable', true);
+    
+            pEl.appendChild(pText);
+            boardDiv.appendChild(pEl);
+    
+            div.appendChild(boardDiv);
+        }
+    })
+}
+
 //DropZones
 
 const dropzones = document.querySelectorAll('.dropzone');
 
-dropzones.forEach(zone => zone.addEventListener('dragleave', dragleave))
+dropzones.forEach(zone => {
+    zone.addEventListener('dragleave', dragleave)
+})
 
 function dragleave(){
     const cardBeingDragged = document.querySelector('.is-dragging');
     
     this.appendChild(cardBeingDragged);
+    save();
 }
 
 //Cards
 
-const blocos = document.querySelectorAll('.bloco');
+let blocos = document.querySelectorAll('.bloco');
 
 blocos.forEach(card => {
     card.addEventListener('dragstart', dragstart)
@@ -27,4 +87,70 @@ function dragstart(){
 function dragend(){
 
     this.classList.remove('is-dragging');
+}
+
+//Save Function
+
+function save(){
+    dropzones.forEach((zone, index) => {
+        if(index === 0){
+            fktodo = zone.children;
+        }
+
+        if(index === 1){
+            fkdoing = zone.children;
+        }
+
+        if(index === 2){
+            fkdone = zone.children;
+        }
+    })
+
+    todo = [];
+    for(let i = 0; i < fktodo.length; i++){
+        todo.push(fktodo[i].children[0].innerHTML);
+    }
+
+    doing = [];
+    for(let i = 0; i < fkdoing.length; i++){
+        doing.push(fkdoing[i].children[0].innerHTML);
+    }
+
+    done = [];
+    for(let i = 0; i < fkdone.length; i++){
+        done.push(fkdone[i].children[0].innerHTML);
+    }
+
+    localStorage.setItem('todo', todo);
+    localStorage.setItem('doing', doing);
+    localStorage.setItem('done', done);
+}
+
+//Add board
+
+let btnBoard = document.getElementById('btn-board');
+
+btnBoard.onclick = () => {
+    let savedTodos = localStorage.getItem('todo');
+
+    let boardTitle = document.getElementById('textBoard').value;
+
+    let aux = [savedTodos, boardTitle];
+
+    localStorage.setItem('todo', aux);
+
+    let abcDiv = document.getElementById('todo');
+    let boardDiv = document.createElement('div');
+    let pEl = document.createElement('p');
+    let pText = document.createTextNode(boardTitle);
+
+    boardDiv.setAttribute('class', 'bloco');
+    boardDiv.setAttribute('draggable', true);
+
+    pEl.appendChild(pText);
+    boardDiv.appendChild(pEl);
+    
+    abcDiv.appendChild(boardDiv);
+    boardDiv.addEventListener('dragstart', dragstart)
+    boardDiv.addEventListener('dragend', dragend)
 }
